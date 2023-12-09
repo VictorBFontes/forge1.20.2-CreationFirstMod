8d22a386da31c6fd0b3ccd78a487901ba901db14
José Victor <victor.jose.barros.fontes@gmail.com>
2023-12-09 16:57:57 -0300

    test commit!!

--- src/main/java/net/victor/tutorialmod/TutorialMod.java
+++ src/main/java/net/victor/tutorialmod/TutorialMod.java
@@ -20,8 +20,7 @@ import org.slf4j.Logger;
 public class TutorialMod {
     // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "firstmod";
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();;
+    public static final Logger LOGGER = LogUtils.getLogger();;
 
     public TutorialMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
